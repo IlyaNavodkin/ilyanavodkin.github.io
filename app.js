62a09d9e24ea0395b014f6a79c1a7bba01e03a9f
Ilia Khudoshin <ilya.sector.1@gmail.com>
2023-12-10 02:49:05 +0300

    refactor

--- app.js
+++ app.js
@@ -1,52 +1,102 @@
+// const status = document.getElementById("status");
+// const messages = document.getElementById("messages");
+// const form = document.getElementById("form");
+// const input = document.getElementById("input");
+
+// let ws;
+
+// function connectWebSocket() {
+//     ws = new WebSocket("ws://79.132.139.166:3001");
+//     ws.onopen = () => {
+//         ws.send("Message"); // Исправление здесь
+//         setStatus("ONLINE. Событие onopen");
+//     };
+
+//     ws.onclose = () => {
+//         setStatus("OFFLINE. Событие onclose, переподключение через 5 секунд");
+//         // Переподключение через 5 секунд
+//         setTimeout(() => {
+//             console.log("Попытка переподключения...");
+
+//             connectWebSocket();
+//         }, 5000);
+//     };
+
+//     ws.onmessage = (response) => {
+//         const data = response.data;
+//         printMessage(data);
+//     };
+
+//     ws.onerror = (error) => {
+//         setStatus("ERROR. Событие onerror");
+//         console.log(error);
+//     };
+// }
+
+// function setStatus(value) {
+//     status.innerHTML = value;
+// }
+
+// function printMessage(value) {
+//     const li = document.createElement("li");
+//     li.innerHTML = value;
+//     messages.appendChild(li);
+// }
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     if (ws.readyState === WebSocket.OPEN) {
+//         const data = input.value.toString();
+//         ws.send(data);
+//         console.log(input.value);
+//         input.value = "";
+//     } else {
+//         console.log("Соединение еще не установлено. Пожалуйста, подождите.");
+//     }
+// });
+
+// // Инициализация соединения
+// connectWebSocket();
+
 const status = document.getElementById("status");
 const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
-let ws;
+const ws = new WebSocket("ws://79.132.139.166:3001");
 
-function connectWebSocket() {
-    ws = new WebSocket("ws://79.132.139.166:3000");
-    ws.onopen = () => {
-        setStatus("ONLINE. Событие onopen");
-    };
+ws.addEventListener("open", () => {
+    setStatus("Connected");
+});
 
-    ws.onclose = () => {
-        setStatus("OFFLINE. Событие onclose, переподключение через 5 секунд");
-        // Переподключение через 5 секунд
-        setTimeout(() => {
-            console.log("Попытка переподключения...");
-            connectWebSocket();
-        }, 5000);
-    };
+ws.addEventListener("message", (event) => {
+    printMessage(event.data);
+});
 
-    ws.onmessage = (response) => {
-        const data = response.data;
+ws.addEventListener("close", () => {
+    setStatus("Connection closed");
+});
 
-        printMessage(data);
-    };
-}
+ws.addEventListener("error", (error) => {
+    setStatus("Error: " + error.message);
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = input.value.trim();
+    if (data) {
+        ws.send(data);
+        input.value = "";
+    }
+});
 
 function setStatus(value) {
-    status.innerHTML = value;
+    status.innerHTML = "Status: " + value;
 }
 
 function printMessage(value) {
     const li = document.createElement("li");
-
-    li.innerHTML = value;
+    li.textContent = value;
     messages.appendChild(li);
 }
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const data = input.value.toString();
-    ws.send(data);
-    console.log(input.value);
-
-    input.value = "";
-});
-
-// Инициализация соединения
-connectWebSocket();
--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const WebSocket = require("ws");
+const http = require("http");
 
-const server = new WebSocket.Server({ port: 3000 });
+// Создаем HTTP-сервер
+const httpServer = http.createServer((req, res) => {
+    if (req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Connect есть)\n");
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found\n");
+    }
+});
 
+httpServer.listen(3001, "79.132.139.166", () => {
+    console.log(`HTTP-сервер запущен на порту ${httpServer.address().port} `);
+});
+
+const server = new WebSocket.Server({ noServer: true }, () => {
+    console.log(`Сервер запущен! ${server.address().port}`);
+    console.log(`Хост ${server.address().address}`);
+});
+
+// Привязываем WebSocket-сервер к HTTP-серверу
 server.on("connection", (ws, req) => {
     try {
-        console.log("Подключение новог клиента");
+        console.log("Подключение нового клиента");
 
         ws.on("message", (message, isBinary) => {
-            console.log(`Сообщение из клиента:`);
+            console.log(`Сообщение от клиента:`);
             console.log(message);
 
             const isBinaryMessage = isBinary ? "Байтовое сообщение" : "Текстовое сообщение";
             console.log(isBinaryMessage);
 
-            const clients = server.clients;
-
-            clients.forEach((client) => {
+            // Отправляем сообщение каждому клиенту
+            server.clients.forEach((client) => {
                 if (client.isAlive === false) {
                     return ws.terminate();
                 }
@@ -33,8 +52,19 @@ server.on("connection", (ws, req) => {
             console.log("Клиент отключился");
         });
 
+        ws.on("error", (error) => {
+            console.log(error);
+        });
+
         ws.send("Добро пожаловать в мой чат!");
     } catch (error) {
         console.log(error);
     }
+});
+
+// Привязываем WebSocket-сервер к HTTP-серверу
+httpServer.on("upgrade", (request, socket, head) => {
+    server.handleUpgrade(request, socket, head, (ws) => {
+        server.emit("connection", ws, request);
+    });
 });
